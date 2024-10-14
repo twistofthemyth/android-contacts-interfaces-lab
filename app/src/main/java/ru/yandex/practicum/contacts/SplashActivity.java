bb7b8408e08077f211b6e71c51ba7ca8cf4ae75f
twistofthemyth <twist.of.the.myth@gmail.com>
2024-10-14 14:05:28 +0300

    Sprint 4 Lesson 5 solution

--- app/src/main/java/ru/yandex/practicum/contacts/SplashActivity.java
+++ app/src/main/java/ru/yandex/practicum/contacts/SplashActivity.java
@@ -33,7 +33,12 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = SplashActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.settingsButton.setOnClickListener(view -> navigateToSettings());
+        binding.settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navigateToSettings();
+            }
+        });
 
         if (ContextUtils.hasContactPermissions(this)) {
             navigateToMain();
